398b193d3696e646b70957d52277a8ab3e42419e
Ovunda Eche <ovus4real@gmail.com>
2024-05-24 17:20:23 +0000

    Add nextAlphabet event listener to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,12 @@ let currentWord;
 let temporaryAlphabetsArray = [];
 let flashalphabets;
 let nextAlphabet = document.getElementById("alphabet__next");
+
+// Add event listeners
+document.addEventListener('DOMContentLoaded', function () {
+
+    fetchFlashAlphabetsData()
+
+   // Change Alphabet on click
+    nextAlphabet.addEventListener('click', generateAlphabet);
+    })
