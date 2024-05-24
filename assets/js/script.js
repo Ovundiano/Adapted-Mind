374801b3e374b2d04e1bdbd0c87735fd91fb0961
Ovunda Eche <ovus4real@gmail.com>
2024-05-24 15:36:51 +0000

    Add nextAlphabet declaration to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
 // Wait for the DOM to finish loading before running the game
 
 let alphabet = document.getElementsByClassName("alphabet");
+let alphabetNextButton = document.getElementById("alphabet__next");
