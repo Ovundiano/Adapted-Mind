ead6b6b0ab345a10b11be11fc97ccce16011583f
Ovunda Eche <ovus4real@gmail.com>
2024-05-24 16:24:02 +0000

    Add declarations to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,7 @@
 let alphabet = document.getElementsByClassName("alphabet");
 let btnVoiceUs = document.getElementById("voice__option--US");
 let btnVoiceGb = document.getElementById("voice__option--GB");
-
+let currentWord;
+let temporaryAlphabetsArray = [];
+let flashalphabets;
 let nextAlphabet = document.getElementById("alphabet__next");
