608b11ec7d9b31a8f1ae21ea0ff7f03e273fe6ce
Ovunda Eche <ovus4real@gmail.com>
2024-05-24 16:11:56 +0000

    Add btnVoiceUs declaration to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // Wait for the DOM to finish loading before running the game
 
 let alphabet = document.getElementsByClassName("alphabet");
-let alphabetNextButton = document.getElementById("alphabet__next");
+let btnVoiceUs = document.getElementById("voice__option--US");
+let btnVoiceGb = document.getElementById("voice__option--GB");
+let nextAlphabet = document.getElementById("alphabet__next");
