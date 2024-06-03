4cb7faf2c0e60ad4bec95413cd9c461c208fbb9a
Ovunda Eche <ovus4real@gmail.com>
2024-06-03 15:47:05 +0000

    Update changes made to JSON, alphabets.html and script.js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,70 @@
 /* jshint esversion:8 */
 
 // Wait for the DOM to finish loading before running the game
-
 // Add event listeners to button element after getting them
-//Coding Kowledge of EventListener gotten from Tutorial Lessons of Code Institute(https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LM101+3/courseware/2d651bf3f23e48aeb9b9218871912b2e/78f3c10a937c4fe09640c7c0098d16bd/)
+//Coding Kowledge of EventListener () function gotten from Tutorial Lessons of Code Institute(https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LM101+3/courseware/2d651bf3f23e48aeb9b9218871912b2e/78f3c10a937c4fe09640c7c0098d16bd/)
 
-document.addEventListener('DOMContentLoaded', function () {
-    let nextAlphabet = document.getElementById("next-alphabet");
+let alphabets =
 
-    //Idea of fetch () function code gotten from flash_cards(https://github.com/IuliiaKonovalova/flash_cards/blob/main/assets/js/cards.js) and indepth knowledge gotten from geeksforgeeks(https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/?ref=ml_lbp)
-    fetchAlphabetsData();
 
-    //Click Next Alphabet Button to Get Next Alphabet
-    nextAlphabet.addEventListener('click', function() {
-        generateAlphabet()
-        console.log('Next Alphabet Button clicked!');
+    document.addEventListener('DOMContentLoaded', function () {
+        let nextAlphabet = document.getElementById("next-alphabet");
+
+        //Idea of fetch () function code gotten from flash_cards(https://github.com/IuliiaKonovalova/flash_cards/blob/main/assets/js/cards.js) and indepth knowledge gotten from geeksforgeeks(https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/?ref=ml_lbp)
+        fetchAlphabetsData();
+
+        //Click Next Alphabet Button to Get Next Alphabet
+        nextAlphabet.addEventListener('click', function () {
+            console.log("this is next alphabet= ", nextAlphabet)
+            generateAlphabet()
+            console.log(alphabets)
+        });
+
+        //Click Enter Key to Get Next Alphabet
+        nextAlphabet.addEventListener('keydown', function (event) {
+            console.log("this is next alphabet= ", nextAlphabet)
+            if (event.key === 'Enter') {
+                generateAlphabet();
+                console.log(alphabets);
+            }
+        });
+
+        //Click Speaker Icon to Listen To Accents Audio
+        //Idea of speakFunction () code gotten from flash_cards(https://github.com/IuliiaKonovalova/flash_cards/blob/main/assets/js/cards.js) and indepth knowledge gotten from mdn web docs(https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak)
+        let btnAccentsUs = document.getElementById("accents-US");
+        let btnAccentsGb = document.getElementById("accents-GB");
+
+        btnAccentsUs.addEventListener('click', function (event) {
+            speakFunction(event, "US");
+        });
+
+        btnAccentsGb.addEventListener('click', function (event) {
+            speakFunction(event, "GB");
+        });
     });
-
-    //Click Enter Key to Get Next Alphabet
-    nextAlphabet.addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            generateAlphabet();
-        }
-        console.log('Enter Key clicked!');
-    });
-
-    //Click Speaker Icon to Listen To Accents Audio
-    let btnAccentsUs = document.getElementById("accents-US");
-    let btnAccentsGb = document.getElementById("accents-GB");
-
-    btnAccentsUs.addEventListener('click', function (event) {
-        speakFunction(event, "US");
-        console.log('US speaker Icon Clicked');
-
-    });
-    btnAccentsGb.addEventListener('click', function (event) {
-        speakFunction(event, "GB");
-        console.log('GB speaker Icon Clicked');
-    });
-});
 
 /**
  * Link To Fetch Data Stored In the JSON Folder
  */
+//Idea of JSON data storage file gotten from flash_cards(https://github.com/IuliiaKonovalova/flash_cards/blob/main/assets/js/cards.js) and indepth knowledge gotten from w3schools(https://www.w3schools.com/js/js_json.asp)
 async function fetchAlphabetsData() {
+    console.log("fetch alphabet data function called")
     let response = await fetch('assets/js/json/alphabets-data.json');
     alphabets = await response.json();
     generateAlphabet();
+    console.log(alphabets)
+
+
 }
 
 /**
  * Generate Random alphabets from A to Z
  */
-let randomAlphabets = function () {
-    return Math.floor(Math.random() * alphabets.length);
-};
+console.log("this is aplphabets == ", alphabets);
+
+
+let randomAlphabets = Math.floor(Math.random() * alphabets.length);
+console.log(randomAlphabets);
 
 /**
  * Generate Alphabet Funtion after data has been fetched from JSON file
@@ -78,6 +88,8 @@ let generateAlphabet = function () {
 
     currentWord = alphabets[randomIndex].imageSpellingWord;
 };
+
+
 
 /**
  * Add Accents Audio to Click Speaker Icon
